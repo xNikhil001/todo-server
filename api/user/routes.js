0e4a758c227d514a7a4a8c6640dec455d5e8bdb7
xNikhil001 <naikn8113@gmail.com>
2022-08-31 10:09:15 +0530

    removed middleware

--- api/user/routes.js
+++ api/user/routes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const {loginUser,addUser,logout} = require('./controller.js');
 const {restrict} = require('./middleware.js');
 
-router.post("/login",restrict,loginUser);
+router.post("/login",loginUser);
 router.post("/register",addUser);
 router.get("/logout",logout);
 
